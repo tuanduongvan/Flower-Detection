03dbdea68beeb7ccb9541b52f529fcc598c5c744
Anh Tuan <leanhtuan131104@gmail.com>
2024-11-27 13:04:27 +0700

    update link

--- flower_detection/views.py
+++ flower_detection/views.py
@@ -83,8 +83,9 @@ def main(request):
                 image = image.convert('RGB')
                 image_np = np.array(image)
 
-            interpreter = tf.lite.Interpreter(model_path='C:\\Users\\PC\Desktop\\AI project\\PBL4_flower\\Flower-Detection\\detect.tflite')
+            # interpreter = tf.lite.Interpreter(model_path='C:\\Users\\PC\Desktop\\AI project\\PBL4_flower\\Flower-Detection\\detect.tflite')
             # interpreter = tf.lite.Interpreter(model_path='D:/test/Flower (1)/webapp/detect.tflite')
+            interpreter = tf.lite.Interpreter(model_path='/home/rasp/Flower-Detection/detect.tflite')
             interpreter.allocate_tensors()
 
             img = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
